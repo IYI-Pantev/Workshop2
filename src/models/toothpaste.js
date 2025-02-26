b71f7c6588696de26d513cbc0c47f54d878162fc
“Nick <nickpantev33@gmail.com>
2025-02-26 11:54:43 +0200

    Toothpaste class created

--- src/models/toothpaste.js
+++ src/models/toothpaste.js
@@ -3,6 +3,9 @@ import { Product } from './product.js';
 import { Gender } from './gender.js';
 
 export class Toothpaste extends Product {
+
+  #ingredients;
+
   /**
   * @param {string} name
   * @param {string} brand
@@ -11,6 +14,23 @@ export class Toothpaste extends Product {
   * @param {string} ingredients
   */
   constructor(name, brand, price, gender, ingredients) {
-    throw new Error("Not implemented!");
+    super(name, brand, price, gender);
+    this.#setIngredients(ingredients);
   }
+
+  /**
+   * @type {Array}
+   */
+  get ingredients() {
+    return this.#ingredients.slice();
+  }
+
+  // calcium,fluorid
+  #setIngredients(ingr) {
+    if (!Array.isArray(ingr) || ingr.length < 1) {
+      throw new Error('Ingredients should be array of at least one element.');
+    }
+    this.#ingredients = ingr;
+  }
+
 }
